22eb76bccc13d4dfa9e22ef07b115dcb71d7fcfb
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2014-02-04 13:54:05 +0100

    Add TravisCI configuration

---
+++ bin/travis.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# -*- coding: UTF8 -*-
+
+echo "setq cloudstack-host \"http://cloudstack.travis.org\"
+           cloudstack-port \"8096\"
+           cloudstack-apikey \"aaa\"
+           cloudstack-secretkey \"bbb\")" >> test/cloudstack.el
+
